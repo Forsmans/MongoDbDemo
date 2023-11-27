2cfb0f99c8b06f48230a38bd074d99a5da08aca7
Magnus Forsman <forsman.magnus@gmail.com>
2023-11-27 13:57:30 +0100

    Add project files.

---
+++ MongoDbDemo2/Data/MongoCRUD.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDbDemo2.Data
+{
+    internal class MongoCRUD
+    {
+        private IMongoDatabase db;
+
+        public MongoCRUD(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+        }
+
+        //C
+        public void InsertRecord<T>(string table, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+            collection.InsertOne(record);
+        }
+
+        //R
+
+        public List<T> LoadRecords<T>(string table)
+        {
+            var collection = db.GetCollection<T>(table);
+
+            return collection.Find(_ => true).ToList();
+
+        }
+    }
+}
---
+++ MongoDbDemo2/Program.cs
@@ -0,0 +1,38 @@
+﻿using MongoDbDemo2.Data;
+using MongoDbDemo2.Models;
+
+namespace MongoDbDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoCRUD db = new MongoCRUD("AdressBook");
+
+            //C
+            //var person = new PersonModel { 
+            //    FirstName = "Simon", 
+            //    LastName = "Arvidsson",
+            //    Adress = new AdressModel
+            //    {
+            //        City = "Los Angeles",
+            //        Street = "Palo Alto Street 123",
+            //        State = "California",
+            //    }
+            //};
+            //db.InsertRecord("Users", person);
+
+            //R
+            var records = db.LoadRecords<PersonModel>("Users");
+
+            foreach(var record in records)
+            {
+                Console.WriteLine(record.FirstName);
+                if(record.Adress != null)
+                {
+                    Console.WriteLine(record.Adress.Street);
+                }
+            }
+        }
+    }
+}
